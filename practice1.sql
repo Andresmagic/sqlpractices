f1e86c033a079f15bba9fbe25080870281f9bfd4
Andresmagic <A01632990@tec.mx>
2022-04-06 23:58:08 +0000

    practice 1 added

---
+++ practice1.sql
@@ -0,0 +1,13 @@
+-- Exercises 1,2,3
+SELECT school, first_name 
+FROM teachers 
+ORDER BY school, first_name 
+ASC;
+
+SELECT* 
+FROM teachers 
+WHERE first_name LIKE 'S%' AND salary >= 40000;
+
+SELECT* 
+FROM teachers 
+WHERE hire_date >= '2010-01-01' ORDER BY salary ASC;
